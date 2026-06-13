6f7547f7aca4934358f9d6a69a8f22714c48dd66
Shreesha-Rao-K <shreesharao2011@gmail.com>
2026-06-13 15:33:19 +0530

    fix: configure vite to bundle all HTML pages to resolve 404s

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+﻿import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
+        register: resolve(__dirname, 'register.html')
+      }
+    }
+  }
+})
